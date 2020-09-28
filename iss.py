86632c41a1336ba3130aab17ced44544b7537295
Arianna Basha <ariannagbasha@gmail.com>
2020-09-28 17:37:43 -0500

    part b, c, d passing all tests

--- iss.py
+++ iss.py
@@ -3,6 +3,11 @@
 __author__ = 'ariannagbasha a.k.a Gabby collabs: Shanquel and Sondos'
 
 import requests
+import turtle
+import time
+import json
+import urllib.request
+
 
 def astronauts():
     r = requests.get('http://api.open-notify.org/astros.json').json()
@@ -22,8 +27,43 @@ def coordinates():
     return [lon, lat]
 
 
+def turtle_graphics():
+    ls = coordinates()
+    screen = turtle.Screen()
+    screen.setup(720, 360)
+    screen.setworldcoordinates(-180, -90, 180, 90)
+    screen.bgpic('map.gif')
+    screen.register_shape('iss.gif')
+    iss = turtle.Turtle()
+    iss.shape('iss.gif')
+    iss.setheading(90)
+    iss.penup()
+    iss.goto(float(ls[0]), float(ls[1]))
+
+
+def indiana_time():
+    lat_indy = 39.76838
+    lon_indy = -86.15804
+    location_indy = turtle.Turtle()
+    location_indy.penup()
+    location_indy.color('yellow')
+    location_indy.goto(lon_indy, lat_indy)
+    location_indy.dot(5)
+    location_indy.hideturtle()
+    url = 'http://api.open-notify.org/iss-pass.json?'
+    params = f'lat={lat_indy}&lon={lon_indy}'
+    response_indy = urllib.request.urlopen(url + params)
+    result_indy = json.loads(response_indy.read())
+    over_indy = result_indy['response'][1]['risetime']
+    location_indy.write(time.ctime(over_indy), font=('Roboto', 10, 'bold'))
+
+
 def main():
     astronauts()
+    coordinates()
+    turtle_graphics()
+    indiana_time()
+    turtle.done()
 
 
 if __name__ == '__main__':
