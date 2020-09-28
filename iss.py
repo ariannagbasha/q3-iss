05d46f78fe081fbeba6386f69d6ce12ff76ce8f9
Arianna Basha <ariannagbasha@gmail.com>
2020-09-28 17:21:45 -0500

    part a passed basic requirements

--- iss.py
+++ iss.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'ariannagbasha a.k.a Gabby collabs: Shanquel and Sondos'
 
+import requests
+
+def astronauts():
+    r = requests.get('http://api.open-notify.org/astros.json').json()
+    num_peeps = r['number']
+    who_peeps = 'People: '
+    for astros in r['people']:
+        who_peeps += astros['name'] + ' , '
+    print(f'Number of people in the station: {num_peeps}')
+    print(who_peeps)
 
 def main():
-    pass
+    astronauts()
 
 
 if __name__ == '__main__':
